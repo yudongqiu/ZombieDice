d1e78f02bd76e4b28f24f7ddbe52f69acc4905e4
qyd <saviorbruce@gmail.com>
2016-07-13 09:45:41 -0700

    clean cached states with scores higher than 22

--- improve/clean_cache.py
+++ improve/clean_cache.py
@@ -4,13 +4,18 @@ import os, pickle
 import sys
 
 cachefilename = 'cachehigh'
-cache_high_target = float(sys.argv[1])
-
 
 if os.path.exists(cachefilename):
     cachehigh = pickle.load( open(cachefilename,"rb") )
     print('Successfully loaded %d high quality cache data'%len(cachehigh))
-    cachehighnew = {k:v for (k,v) in cachehigh.items() if all(p<20 for p in k[-4])}
+    cachehighnew = {}
+    for k,v in cachehigh.items():
+        bag, dices, players, myidx, goal, me = k
+        players = list(players)
+        if myidx < len(players): 
+            players[myidx] += sum(c[0] for c in dices)
+        if all(p < 22 for p in players):
+            cachehighnew[k] = v
     n_removed = len(cachehigh) - len(cachehighnew)
     if n_removed:
         print('%d states is removed from cachehigh'%(n_removed))
